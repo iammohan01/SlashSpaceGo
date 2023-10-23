c390a7b62e47a394fab0c6f72507a006b84108de
iammohan01 <mohanasundaram7500@gmail.com>
2023-10-23 17:19:04 +0530

    [Clean] font import optimizations

--- src/popup/main.jsx
+++ src/popup/main.jsx
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom/client'
 import {ContextProvider} from "./context/PopupContext.jsx";
 import Popup from "./Popup.jsx";
 import "./styles/popup.css"
+import '@fontsource/poppins/';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <ContextProvider>
