9246b6968f99551d85a1ab5d96d0d5b1cb46cbb4
iammohan01 <mohanasundaram7500@gmail.com>
2023-10-23 15:20:45 +0530

    [Impl] exporting a multiple react files

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 import hotReloadExtension from 'hot-reload-extension-vite';
+import {resolve} from "path"
 
 export default defineConfig({
   plugins: [
@@ -13,5 +14,11 @@ export default defineConfig({
   ],
   build: {
     emptyOutDir: false,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        admin: resolve(__dirname, 'popup.html')
+      }
+    }
   }
 });
