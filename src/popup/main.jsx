b9ae8e07164009270f4bf34a09f8a5c6966c5a5a
iammohan01 <mohanasundaram7500@gmail.com>
2023-12-02 02:30:48 +0530

    fix : project's css adjustment

--- src/popup/main.jsx
+++ src/popup/main.jsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import {ContextProvider} from "./context/PopupContext.jsx";
 import Popup from "./Popup.jsx";
-import "./styles/popup.css"
+import "../styles/popup.css"
 import '@fontsource/poppins/';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
