aa3d328aa5ad79408af3f77931e5fb1521ecf47d
iammohan01 <mohanasundaram7500@gmail.com>
2023-12-03 02:25:12 +0530

    feat : console log eslint configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -20,6 +20,14 @@ module.exports = {
           "args": "after-used",
           "argsIgnorePattern": "^_"
       }],
+      "no-console": "warn",
+      "no-restricted-syntax": [
+          "error",
+          {
+              "selector": "CallExpression[callee.object.name='console'][callee.property.name!=/^(log|warn|error|info|trace)$/]",
+              "message": "Unexpected property on console object was called"
+          }
+      ],
     'react-refresh/only-export-components': ['error', {allowConstantExport: true},],
     "no-unused-expressions": "off",
     "camelcase": ["error"],
