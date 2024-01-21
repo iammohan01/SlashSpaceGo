4f0d06fe61900cfbb04214edb3c2c1642cb6833a
iammohan01 <mohanasundaram7500@gmail.com>
2024-01-21 18:01:27 +0530

    fix: icons changed and icon-width

--- src/popup/Header.tsx
+++ src/popup/Header.tsx
@@ -1,12 +1,12 @@
 import React from "react"
-import light from "/resources/icons/lightnig.svg"
+import icon from "/icons/48_48.png"
 import share from "/resources/icons/share.svg"
 
 export default function Header(): React.ReactElement {
     return (
         <div className="header">
             <div className="icon">
-                <img src="" alt="" srcSet={light}/>
+                <img className={"w-10"} src="" alt="" srcSet={icon}/>
             </div>
             <div className="title">SLASH SPACE GO</div>
             <div className="share">
